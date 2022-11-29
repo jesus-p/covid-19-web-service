60f9437ec4d44b820dd4cc20d0667b48856a67ea
Jesus P <jesus_p@icloud.com>
2022-11-29 06:31:14 -0600

    Added new pages and their respective queries

--- main.py
+++ main.py
@@ -14,52 +14,191 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/brazil')
+def brazil():
+    #initialize query
+    client = bigquery.Client()
+    sql = """
+        SELECT date, SUM(confirmed) num_reports
+        FROM `bigquery-public-data.covid19_open_data.compatibility_view`
+        WHERE country_region = 'Brazil'
+        GROUP BY date
+        HAVING num_reports IS NOT NULL
+        ORDER BY date DESC
+        LIMIT 20
+    """
+    df = client.query(sql).to_dataframe()
+
+    fig = px.line(df, x="date", y="num_reports",title='Confirmed COVID-19 Cases in Brazil')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    header="Brazil"
+    description = """
+    Showing live statistics for Brazil
+    """
+    return render_template('chart.html', graphJSON=graphJSON, header=header,description=description)
+
+@app.route('/china')
+def china():
+    #initialize query
+    client = bigquery.Client()
+    sql = """
+        SELECT date, SUM(confirmed) num_reports
+        FROM `bigquery-public-data.covid19_open_data.compatibility_view`
+        WHERE country_region = 'China'
+        GROUP BY date
+        HAVING num_reports IS NOT NULL
+        ORDER BY date DESC
+        LIMIT 20
+    """
+    df = client.query(sql).to_dataframe()
+
+    fig = px.line(df, x="date", y="num_reports",title='Confirmed COVID-19 Cases in China')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    header="China"
+    description = """
+    Showing live statistics for China
+    """
+    return render_template('chart.html', graphJSON=graphJSON, header=header,description=description)
+
 @app.route('/mexico')
 def mexico():
     #initialize query
     client = bigquery.Client()
     sql = """
-        SELECT country_name, population
-        FROM `covid-19-web-service.covid19data.covid19_open_data` 
-        LIMIT 10
+        SELECT date, SUM(confirmed) num_reports
+        FROM `bigquery-public-data.covid19_open_data.compatibility_view`
+        WHERE country_region = 'Mexico'
+        GROUP BY date
+        HAVING num_reports IS NOT NULL
+        ORDER BY date DESC
+        LIMIT 20
     """
     df = client.query(sql).to_dataframe()
 
-    fig = px.line(df, x="country_name", y="population",title='Population in Mex')
+    fig = px.line(df, x="date", y="num_reports",title='Confirmed COVID-19 Cases in Mexico')
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     
     header="Mexico"
     description = """
-    test
+    Showing live statistics for Mexico
     """
     return render_template('chart.html', graphJSON=graphJSON, header=header,description=description)
 
-@app.route('/usa')
-def usa():
-    df = pd.DataFrame({
-        "Vegetables": ["Lettuce", "Cauliflower", "Carrots", "Lettuce", "Cauliflower", "Carrots"],
-        "Amount": [10, 15, 8, 5, 14, 25],
-        "City": ["London", "London", "London", "Madrid", "Madrid", "Madrid"]
-    })
+@app.route('/jordan')
+def jordan():
+    #initialize query
+    client = bigquery.Client()
+    sql = """
+        SELECT date, SUM(confirmed) num_reports
+        FROM `bigquery-public-data.covid19_open_data.compatibility_view`
+        WHERE country_region = 'Jordan'
+        GROUP BY date
+        HAVING num_reports IS NOT NULL
+        ORDER BY date DESC
+        LIMIT 20
+    """
+    df = client.query(sql).to_dataframe()
 
-    fig = px.bar(df, x="Vegetables", y="Amount", color="City", barmode="stack")
-
+    fig = px.line(df, x="date", y="num_reports",title='Confirmed COVID-19 Cases in Jordan')
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    header="USA"
+    
+    header="Jordan"
     description = """
-    ****** USA INFO
+    Showing live statistics for Jordan
+    """
+    return render_template('chart.html', graphJSON=graphJSON, header=header,description=description)
+
+@app.route('/india')
+def india():
+    #initialize query
+    client = bigquery.Client()
+    sql = """
+        SELECT date, SUM(confirmed) num_reports
+        FROM `bigquery-public-data.covid19_open_data.compatibility_view`
+        WHERE country_region = 'India'
+        GROUP BY date
+        HAVING num_reports IS NOT NULL
+        ORDER BY date DESC
+        LIMIT 20
+    """
+    df = client.query(sql).to_dataframe()
+
+    fig = px.line(df, x="date", y="num_reports",title='Confirmed COVID-19 Cases in India')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    header="India"
+    description = """
+    Showing live statistics for India
+    """
+    return render_template('chart.html', graphJSON=graphJSON, header=header,description=description)
+
+@app.route('/italy')
+def italy():
+    #initialize query
+    client = bigquery.Client()
+    sql = """
+        SELECT date, SUM(confirmed) num_reports
+        FROM `bigquery-public-data.covid19_open_data.compatibility_view`
+        WHERE country_region = 'Italy'
+        GROUP BY date
+        HAVING num_reports IS NOT NULL
+        ORDER BY date DESC
+        LIMIT 20
+    """
+    df = client.query(sql).to_dataframe()
+
+    fig = px.line(df, x="date", y="num_reports",title='Confirmed COVID-19 Cases in Italy')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    header="Italy"
+    description = """
+    Showing live statistics for Italy
+    """
+    return render_template('chart.html', graphJSON=graphJSON, header=header,description=description)
+
+@app.route('/peru')
+def peru():
+    #initialize query
+    client = bigquery.Client()
+    sql = """
+        SELECT date, SUM(confirmed) num_reports
+        FROM `bigquery-public-data.covid19_open_data.compatibility_view`
+        WHERE country_region = 'Peru'
+        GROUP BY date
+        HAVING num_reports IS NOT NULL
+        ORDER BY date DESC
+        LIMIT 20
+    """
+    df = client.query(sql).to_dataframe()
+
+    fig = px.line(df, x="date", y="num_reports",title='Confirmed COVID-19 Cases in Peru')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    header="Peru"
+    description = """
+    Showing live statistics for Peru
     """
     return render_template('chart.html', graphJSON=graphJSON, header=header,description=description)
 
 @app.route('/case_study')
 def casestudy():
-    header="Let's plan a trip to the 7 Wonders of the World!"
+    header="Let's Plan a Trip to The 7 Wonders of The World!"
     
-    description = """
-    A recent Vrbo survey of 8,000-plus people found that 65 percent of Americans plan on traveling more in 2022 than they did pre-Covid. Traveling after lockdown will look much different than before. 
-    International travel poses additional risks, and even fully vaccinated travelers might be at increased risk for getting and possibly spreading some COVID-19 variants.
-    It is now extremely important to be aware of the dangers of COVID19 when travelling.  Our group decided that a visual representation of these dangers, in terms of how many reported cases of COVID19 there is internationally,
-    would help travellers be safer when taking trips internationally. We created a Google App Engine that uses Machine Learning to help discern this informatio when considering taking a trip to one of the Seven Wonders of the World.
-    The New Seven Wonders of the Worlds are:
-    """
-    return render_template('webpage.html', header=header,description=description)
+    return render_template('webpage.html', header=header)
+
+@app.route('/code_snippet')
+def code():
+    return render_template('code.html')
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
